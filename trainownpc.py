bb00f9e3e8ecde7778116e9bd4f2471c9d3e8e11
Jarro <231667@buas.nl>
2025-01-09 12:09:39 +0100

    add

--- trainownpc.py
+++ trainownpc.py
@@ -11,7 +11,6 @@ from stable_baselines3.common.callbacks import EvalCallback, CallbackList
 from stable_baselines3.common.env_checker import check_env
 import gymnasium as gym
 import argparse
-from clearml import Task
 import wandb
 import typing_extensions as TypeIs
 import tensorflow
@@ -21,16 +20,6 @@ from ot2_env_wrapper_rework3 import OT2Env
 # Load the API key for wandb
 os.environ['WANDB_API_KEY'] = 'c9df9474ff41da6be45ba2e10ff13f6749aec8dd'
 run = wandb.init(project="2024-Y2B-RoboSuite", sync_tensorboard=True)
-
-task = Task.init(
-    project_name="Mentor Group D/Group 3/JarroTeunissen",  # Replace YourName with your own name
-    task_name="Experiment1",
-    
-)
-
-# Set base docker image and queue
-task.set_base_docker("deanis/2023y2b-rl:latest")
-task.execute_remotely(queue_name="default")
 
 #Define the model
 env = OT2Env()
@@ -50,8 +39,6 @@ parser.add_argument("--learning_rate", type=float, default=0.0001)
 parser.add_argument("--batch_size", type=int, default=32)
 parser.add_argument("--n_steps", type=int, default=2048)
 parser.add_argument("--n_epochs", type=int, default=10)
-
-
 
 # Extra parameters chosen because of the training
 parser.add_argument("--gamma", type=float, default=0.98)
@@ -74,16 +61,15 @@ model = PPO(args.policy, env, verbose=1,
 # Callback for wandb
 wandb_callback = WandbCallback(
     model_save_freq=100000,
-    model_save_path=f"models/{run.id}",
+    model_save_path=f"modelspc/{run.id}",
     verbose=2,)
 
 # Train the model
 model.learn(total_timesteps=timesteps, callback=wandb_callback, progress_bar=True, reset_num_timesteps=False,tb_log_name=f"runs/{run.id}")
 # Save the model.
-model.save(f"models/{run.id}/{timesteps}_baseline")
+#model.save(f"models/{run.id}/{timesteps}_baseline")
 # Save the model to wandb
 wandb.save(f"models/{run.id}/{timesteps}_baseline")
 
-
 # python train.py --learning_rate 0.0001 --batch_size 32 --n_steps 2048 --n_epochs 10 --gamma 0.98 --policy MlpPolicy --clip_range 0.15 --value_coefficient 0.5
-#python train.py --learning_rate 0.0003 --batch_size 64 --n_steps 2048 --n_epochs 10 --gamma 0.99 --policy MlpPolicy --clip_range 0.2 --value_coefficient 0.5
+#python trainownpc.py --learning_rate 0.0003 --batch_size 64 --n_steps 2048 --n_epochs 10 --gamma 0.99 --policy MlpPolicy --clip_range 0.2 --value_coefficient 0.5
